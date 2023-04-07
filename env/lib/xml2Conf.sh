161c8eb368713bc0c03cd5283b8b9cae2678f2e6
Istiak Morsalin <anik.cse@gmail.com>
2023-04-08 00:40:40 +0600

    another commit

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/lib"
+XML2_LIBS="-lxml2 -L/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/lib -lz -L/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/lib -llzma -lpthread -L/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/lib -liconv -L/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/include/libxml2 -I/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
---
+++ env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/lib"
+XSLT_LIBS="-lxslt -L/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/lib -lxml2 -L/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/lib -L/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/lib -lpthread -L/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/lib -liconv -L/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/lib -licui18n -licuuc -licudata -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/istiakmorsalin/iOffice/ml_projects/sample_project/env/include"
+MODULE_VERSION="xslt-1.1.35"
